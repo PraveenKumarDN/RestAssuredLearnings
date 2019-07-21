5ab3a5f2e0ca38703d9755ef1ae5ee04916fa131
Praveen Kumar <praveen.kumar@freecharge.com>
2019-07-21 19:07:57 +0530

    commiting Learnings 1 2 3 4 classes as part of rest api basic learnings

--- src/test/java/Learnings3LogsForDebugging.java
+++ src/test/java/Learnings3LogsForDebugging.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
@@ -7,7 +8,6 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -19,7 +19,7 @@ import static org.hamcrest.Matchers.equalTo;
  * This class contains the simple GET and POST rest calls java automation.
  * Note: RestAssured consists of 4 blocks: given(), when(), then(), extract()..
  */
-public class Learnings2Centralisations {
+public class Learnings3LogsForDebugging {
     private String host = null ;
 
     @BeforeClass
@@ -34,8 +34,7 @@ public class Learnings2Centralisations {
     }
 
     @Test
-    public void createPlaceAPI()
-    {
+    public void createDeletePlaceAPIWithLogsForEasyDebuggingInAutomation() {
         RestAssured.baseURI=host;
 
         //case1: post operation to create add place api in google maps
@@ -53,17 +52,19 @@ public class Learnings2Centralisations {
                         "\"types\": [\"shoe_store\"],"+
                         "\"website\": \"http://www.google.com.au/\","+
                         "\"language\": \"en-AU\""+
-                        "}").
+                        "}").log().method().log().uri().
                 when().
                 post("/maps/api/place/add/json").
                 then().assertThat().statusCode(200).and().contentType(ContentType.JSON).and().
-                body("status",equalTo("OK")).
+                body("status",equalTo("OK")).log().ifValidationFails(LogDetail.ALL).
                 extract().
                 response();
 
         //converting raw to string response
+        System.out.println("----------------------------------------------------");
         String stringResponse = response.asString() ;
-        System.out.println("Post api create call response: "+ stringResponse);
+        System.out.println("Post api create call response:\n "+ stringResponse);
+        System.out.println("----------------------------------------------------");
 
         //parsing the string response to json string. More info: https://jsoneditoronline.org/
         JsonPath jsonPath = new JsonPath(stringResponse);
@@ -83,9 +84,10 @@ public class Learnings2Centralisations {
                 extract().
                 response();
 
+        System.out.println("----------------------------------------------------");
         String deletedStringResponse = responseDelete.asString() ;
         System.out.println("Post api delete call response: "+ deletedStringResponse);
-
+        System.out.println("----------------------------------------------------");
     }
 
 }
--- src/test/java/Learnings4XMLResponse.java
+++ src/test/java/Learnings4XMLResponse.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
@@ -7,7 +8,6 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -17,9 +17,9 @@ import static org.hamcrest.Matchers.equalTo;
 /**
  * @author praveenkumar
  * This class contains the simple GET and POST rest calls java automation.
- * Note: RestAssured consists of 4 blocks: given(), when(), then(), extract()..
+ * Note: Not working properly but code has been return for xml string convertion.
  */
-public class Learnings2Centralisations {
+public class Learnings4XMLResponse {
     private String host = null ;
 
     @BeforeClass
@@ -30,40 +30,36 @@ public class Learnings2Centralisations {
         FileInputStream fileInputStream = new FileInputStream(file) ;
         properties.load(fileInputStream);
         host = properties.getProperty("host") ;
+    }
+
+    //This method helps in generating the xml String for xml payload path passed as argument
+    private String generateStringFromResource(String path) throws IOException{
+        //return new String(Files.readAllBytes(Paths.get(path)));
+        return null ;
 
     }
 
     @Test
-    public void createPlaceAPI()
-    {
+    public void createDeletePlaceAPIWithXMLResponse() throws IOException {
         RestAssured.baseURI=host;
 
+        String xmlRequest = generateStringFromResource("/home/praveenkumar/workspace/restapi/src/main/resources/files/postData.xml") ;
         //case1: post operation to create add place api in google maps
         Response response = given().
                 queryParam("key","qaclick123").
-                body("{"+
-                        "\"location\": {"+
-                        "\"lat\": -33.8669710,"+
-                        "\"lng\": 151.1958750"+
-                        "},"+
-                        "\"accuracy\": 50,"+
-                        "\"name\": \"Google Shoes!\","+
-                        "\"phone_number\": \"(02) 9374 4000\","+
-                        "\"address\": \"48 Pirrama Road, Pyrmont, NSW 2009, Australia\","+
-                        "\"types\": [\"shoe_store\"],"+
-                        "\"website\": \"http://www.google.com.au/\","+
-                        "\"language\": \"en-AU\""+
-                        "}").
+                body(xmlRequest).log().method().log().uri().
                 when().
                 post("/maps/api/place/add/json").
                 then().assertThat().statusCode(200).and().contentType(ContentType.JSON).and().
-                body("status",equalTo("OK")).
+                body("status",equalTo("O")).log().ifValidationFails(LogDetail.ALL).
                 extract().
                 response();
 
         //converting raw to string response
+        System.out.println("----------------------------------------------------");
         String stringResponse = response.asString() ;
-        System.out.println("Post api create call response: "+ stringResponse);
+        System.out.println("Post api create call response:\n "+ stringResponse);
+        System.out.println("----------------------------------------------------");
 
         //parsing the string response to json string. More info: https://jsoneditoronline.org/
         JsonPath jsonPath = new JsonPath(stringResponse);
@@ -83,9 +79,10 @@ public class Learnings2Centralisations {
                 extract().
                 response();
 
+        System.out.println("----------------------------------------------------");
         String deletedStringResponse = responseDelete.asString() ;
         System.out.println("Post api delete call response: "+ deletedStringResponse);
-
+        System.out.println("----------------------------------------------------");
     }
 
 }
